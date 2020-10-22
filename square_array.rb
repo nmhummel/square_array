120ea639340af0406d03b91a57f0040e6abd0b0a
nmhummel <natalie.hummel@gmail.com>
2020-10-21 21:42:03 -0500

    commit tree 8c0d71cb1cf46e515e74c247ee22c66c9254891f

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
+# to square a number use ** => x**y / in this case, x**x
+#  array = 1,2,3
+
+
+
+def square_array(array)           
+    new_array_squared = []
+    array.each do |number|    
+      new_array_squared.push number**2  
+    end
+  return new_array_squared        
 end
+
